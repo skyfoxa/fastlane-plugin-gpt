9e8f3bb1226adc95f207e902bc0bfd8438600f47
Marek Zvara <marek724@gmail.com>
2023-04-11 10:21:27 +0200

    update gpt_action with the lane script

--- lib/fastlane/plugin/gpt/actions/gpt_action.rb
+++ lib/fastlane/plugin/gpt/actions/gpt_action.rb
@@ -5,7 +5,25 @@ module Fastlane
   module Actions
     class GptAction < Action
       def self.run(params)
-        UI.message("The gpt plugin is working!")
+          Actions.verify_gem!('openai')
+          require 'openai'
+          
+          api_key = params[:api_key]
+          changelog = params[:changelog]
+          prompt = params[:prompt]
+          
+          UI.message("Running prompt: '" + prompt + "'")
+          request = prompt + " " + changelog
+          
+          client = OpenAI::Client.new(access_token: api_key)
+          
+          response = client.chat(
+              parameters: {
+                  model: "gpt-3.5-turbo",
+                  messages: [{ role: "user", content: request}]
+              })
+          
+          response.dig("choices", 0, "message", "content")
       end
 
       def self.description
@@ -27,11 +45,29 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "GPT_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+        FastlaneCore::ConfigItem.new(key: :api_key,
+                                env_name: "FASTGPT_API_KEY",
+                             description: "Pass api key for Chat GPT",
+                                    type: String,
+                                     verify_block: proc do |value|
+                                                 UI.user_error!("No api key") if value.to_s.length == 0
+                                 end),
+        FastlaneCore::ConfigItem.new(key: :changelog,
+                                env_name: "FASTGPT_CHANGELOG",
+                             description: "Changelog, GPT will generate release notes from",
+                                optional: false,
+                                    type: String,
+                                     verify_block: proc do |value|
+                                                 UI.user_error!("No changelog provided") if value.to_s.length == 0
+                                 end),
+         FastlaneCore::ConfigItem.new(key: :prompt,
+                                 env_name: "FASTGPT_PROMPT",
+                              description: "Prompt, e.g. Create release notes for AppStore from: ",
+                                 optional: false,
+                                     type: String,
+                                      verify_block: proc do |value|
+                                                  UI.user_error!("No prompt provided") if value.to_s.length == 0
+                                  end)
         ]
       end
 
